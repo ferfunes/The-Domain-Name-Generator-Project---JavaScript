4b92012c7ebdbaaaad6f3a1e714a6b15bec264a0
Fernando Funez <ferfunes@gmail.com>
2019-08-24 17:34:37 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,19 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#dGenerator").innerHTML = generateDomain();
+
+  function generateDomain() {
+    var pronound = ["the", "our"];
+    var adj = ["great", "big"];
+    var noun = ["jogger", "racoon"];
+
+    for (var i = 0; i < pronound.length; i++) {
+      for (var y = 0; y < adj.length; y++) {
+        for (var j = 0; j < noun.length; j++) {
+          return "www." + pronound[i] + adj[y] + noun[j] + ".com ";
+        }
+      }
+    }
+  }
 };
